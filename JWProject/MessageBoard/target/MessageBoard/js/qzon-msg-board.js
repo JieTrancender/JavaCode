4e5ec4f88e793d01e6cfc1b8e854a5a7c0309495
Jason <jie-email@jie-trancender.org>
2017-04-11 16:43:52 +0800

    update

--- JWProject/MessageBoard/target/MessageBoard/js/qzon-msg-board.js
+++ JWProject/MessageBoard/target/MessageBoard/js/qzon-msg-board.js
@@ -34,7 +34,7 @@ $(function(){
         var email = 'jie-email@jie-trancender.org';
         var title = 'Msg from qzon like msg board';
         var content = $('.comment_editor textarea').val();
-        var time = new Date().toString();
+        var time = new Date().toString().replace("GMT", "");
         var data = 'name=' + name + '&phone=' + phone + '&email=' + email + '&title=' + title + '&content=' + content + '&time=' + time;
         submit(data);
         getMsgs();
