8f247b351cca9aff859071fbaebbfeb763227acf
Jason <jie-email@jie-trancender.org>
2017-06-23 13:35:31 +0800

    update common-tools

---
+++ multi-module/common-tools/src/main/java/jason/common/tools/JsonUtils.java
@@ -0,0 +1,11 @@
+package jason.common.tools;
+
+/**
+ * Created by JTrancender on 2017/6/22.
+ */
+public class JsonUtils {
+    public static<T> Object JsonToObject(String jsonStr, Class<T> obj) {
+        T t = null;
+        return t;
+    }
+}
